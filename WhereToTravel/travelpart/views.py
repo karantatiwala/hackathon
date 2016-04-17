f6cd50342f2b86782205d713bbc6b03220534619
Himanshi-Khandelwal <himanshikhandelwal8@gmail.com>
2016-04-17 15:42:34 +0530

    commit 3

--- WhereToTravel/travelpart/views.py
+++ WhereToTravel/travelpart/views.py
@@ -1,28 +1,23 @@
+
+from settings import GOOGLE_API_KEY
+from django import forms
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
-import urllib, json
+from googleplaces import GooglePlaces
+lass NameForm(forms.Form):
+    your_location = forms.CharField(label='Your name', max_length=100)
+def getPlaces(location,keyWord,Radius):
+	google_places = GooglePlaces(GOOGLE_API_KEY)
+	# You may prefer to use the text_search API, instead.
+	query_result = google_places.nearby_search(
+		location=location, keyword="landmarks",
+		radius=500, types=[types.TYPE_place_of_worship,types.TYPE_zoo,types.TYPE_,types.TYPE_natural_feature,types.TYPE_city_hall])
 
-#Grabbing and parsing the JSON data
-def GoogPlac(lat,lng,radius,types,key):
-  #making the url
-  AUTH_KEY = key
-  LOCATION = str(lat) + "," + str(lng)
-  RADIUS = radius
-  TYPES = types
-  MyUrl = ('https://maps.googleapis.com/maps/api/place/nearbysearch/json'
-           '?location=%s'
-           '&radius=%s'
-           '&types=%s'
-           '&sensor=false&key=%s') % (LOCATION, RADIUS, TYPES, AUTH_KEY)
-  #grabbing the JSON result
-  response = urllib.urlopen(MyUrl)
-  jsonRaw = response.read()
-  jsonData = json.loads(jsonRaw)
-  return jsonData
-
-#This is a helper to grab the Json data that I want in a list
-def IterJson(place):
-  x = [place['name'], place['reference'], place['geometry']['location']['lat'], 
-         place['geometry']['location']['lng'], place['vicinity']]
-  return x
+def TravelOutput(request) :
+	if(request.method=='POST'):
+		if form.is_valid():
+			form = NameForm(request.POST)
+            		getPlaces(form.your_location)
+			
+	return render(request, 'name.html', {'form': form})
